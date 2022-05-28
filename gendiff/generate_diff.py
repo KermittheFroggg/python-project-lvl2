7dcf1c8fbce262e07c79c7da8c1cf194621c347d
KermittheFroggg <santamantana@gmail.com>
2022-05-28 14:37:25 +0300

    step2

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,24 +1,18 @@
 import json
 
-def generate_diff(file_path1, file_path2, format=json):
-    a = json.load(open(file_path1))
-    b = json.load(open(file_path2))
-    dif_keys_a = set(a) - set(b)
-    dif_keys_b = set(b) - set(a)
-    same_keys = set(a) & set(b)
-    source_a = ''
-    source_b = ''
-    source_c = ''
-    for i in dif_keys_a:
-        source_a = source_a + "\n" + ' - ' + str(i) + ': ' + str(a[i])
-    for i in same_keys:
-        if a[i] == b[i]:
-            source_a = source_a + "\n" + '   ' + str(i) + ': ' + str(a[i])
+
+def generate_diff(file_path1, file_path2):
+    file1 = json.load(open(file_path1))
+    file2 = json.load(open(file_path2))
+    source = ''
+    for elem in (set(file1) - set(file2)):
+        source += f'\n - {elem}: {file1[elem]}'
+    for elem in (set(file1) & set(file2)):
+        if file1[elem] == file2[elem]:
+            source += f'\n   {elem}: {file1[elem]}'
         else:
-            source_c = "\n" + ' - ' + str(i) + ': ' + str(a[i]) + "\n" + ' + ' + str(i) + ': ' + str(b[i])
-    for i in dif_keys_b:
-        source_b = source_b + "\n" + ' + ' + str(i) + ': ' + str(b[i])
-    result_a = '{' + source_a 
-    result_b = source_b + "\n" + '}'
-    result = result_a + source_c + result_b
-    return result
+            source += f'\n - {elem}: {file1[elem]}\n + {elem}: {file2[elem]}'
+    for elem in (set(file2) - set(file1)):
+        source += f'\n + {elem}: {file2[elem]}'
+    source = '{' + source + '\n' + '}'
+    return source
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
+
 import argparse
+
 from gendiff.generate_diff import generate_diff
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser = argparse.ArgumentParser(
+        description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-    print(generate_diff(args.__dict__['first_file'], args.__dict__['second_file']))
+    print(generate_diff(
+        args.__dict__['first_file'], args.__dict__['second_file']))
+
 
 if __name__ == '__main__':
     main()
