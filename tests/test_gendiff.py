281fda79b19e0294ef06f661b5c772566b3700c0
KermittheFroggg <santamantana@gmail.com>
2022-05-28 20:40:02 +0300

    step4

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,7 +3,7 @@ from gendiff.generate_diff import generate_diff
 
 def test_gendiff():
     result = generate_diff('tests/fixtures/file_path1.json', 'tests/fixtures/file_path2.json')
-    result2 = open('result_json.txt')
+    result2 = open('tests/fixtures/result_json.txt')
     final_result = ''
     for elem in result:
         if elem in result2:
