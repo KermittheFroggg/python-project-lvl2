71f86b00577e81ba62017003a3207c65e19c639b
KermittheFroggg <santamantana@gmail.com>
2022-05-29 14:25:24 +0300

    step4

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,18 +1,29 @@
 import json
 
 
+def boolTolower(file):
+    lowerbool = {}
+    for key, value in file.items():
+        if isinstance(value, bool):
+            lowerbool[key] = str(value).lower()
+        else:
+            lowerbool[key] = value
+    return lowerbool
+
+
 def generate_diff(file_path1, file_path2):
-    file1 = json.load(open(file_path1))
-    file2 = json.load(open(file_path2))
+    file1 = boolTolower(json.load(open(file_path1)))
+    file2 = boolTolower(json.load(open(file_path2)))
+    print(file1)
     source = ''
     for elem in (set(file1) - set(file2)):
-        source += f'\n - {elem}: {file1[elem]}'
+        source += f'\n  - {elem}: {file1[elem]}'
     for elem in (set(file1) & set(file2)):
         if file1[elem] == file2[elem]:
-            source += f'\n   {elem}: {file1[elem]}'
+            source += f'\n    {elem}: {file1[elem]}'
         else:
-            source += f'\n - {elem}: {file1[elem]}\n + {elem}: {file2[elem]}'
+            source += f'\n  - {elem}: {file1[elem]}\n  + {elem}: {file2[elem]}'
     for elem in (set(file2) - set(file1)):
-        source += f'\n + {elem}: {file2[elem]}'
+        source += f'\n  + {elem}: {file2[elem]}'
     source = '{' + source + '\n' + '}'
     return source
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,11 +1,13 @@
 from gendiff.generate_diff import generate_diff
 
-
 def test_gendiff():
-    result = generate_diff('tests/fixtures/file_path1.json', 'tests/fixtures/file_path2.json')
-    result2 = open('tests/fixtures/result_json.txt')
-    final_result = ''
-    for elem in result:
-        if elem in result2:
-            final_result += elem
-    assert result == final_result
+    result_func = generate_diff('tests/fixtures/file_path1.json', 'tests/fixtures/file_path2.json').split('\n')
+    result_text = open('tests/fixtures/result_json.txt', 'r')
+    result_text_corr = []
+    for line in result_text:
+        result_text_corr.append(line.strip('\n'))
+    final_result = []
+    for elem in result_func:
+        if elem in result_text_corr:
+            final_result.append(elem)
+    assert result_func == final_result
