e109026c71ef584bb76d2f048810e4bb626166f8
KermittheFroggg <santamantana@gmail.com>
2022-06-11 22:29:11 +0300

    step8

--- gendiff/plain.py
+++ gendiff/plain.py
@@ -23,6 +23,6 @@ def modernize(elem):
         if elem not in ('true', 'false', 'null'):
             return f"'{elem}'"
         elif isinstance(elem, int):
-            return f"'{elem}'"
+            return int(elem)
         else:
             return elem
