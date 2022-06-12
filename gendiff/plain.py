29b21f979006cbd59c5137f45b4e554f0b2a27d8
KermittheFroggg <santamantana@gmail.com>
2022-06-12 12:35:44 +0300

    step8

--- gendiff/plain.py
+++ gendiff/plain.py
@@ -10,19 +10,18 @@ def plain(diff):
             if not (isinstance(elem[2], dict) and (
                     isinstance(elem[3], dict))) and (
                     elem[2] != elem[3]):
-                result += 'Property ' + modernize(elem[1]) + \
-                    " was updated. From " + \
-                    modernize(elem[2]) + ' to ' + modernize(elem[3]) + '\n'
+                result += f"Property {modernize(elem[1])} was updated." + (
+                    f" From {modernize(elem[2])} to {modernize(elem[3])}\n")
     return result.strip('\n')
 
 
 def modernize(elem):
     if isinstance(elem, dict):
         return '[complex value]'
+    elif isinstance(elem, int):
+        return elem
     else:
         if elem not in ('true', 'false', 'null'):
             return f"'{elem}'"
-        elif isinstance(elem, int):
-            return elem.strip("'")
         else:
             return elem
